f80499e400ebaafa30c808481f4d117cb1f26c5a
git-polesgu <git-polesgu@yandex.ru>
2019-05-14 14:34:48 +0300

    added file 4.cpp

---
+++ 4.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+int main() {
+	int A , B , C ;
+
+	std :: cin >> A;
+	
+	switch (A)
+	{
+		case 0:
+			int Z;
+			Z = 15;
+			std:: cout << "mon" << A << std :: endl;
+			break;
+
+		case 1:
+			int Y = 10;
+			std :: cout << "tue" << Y << std :: endl;
+			break;
+		case 2:
+			std :: cout << "wen" << std :: endl;
+			break;
+		case 3:
+			std :: cout << "two" << std :: endl;
+			break;
+		case 4:
+			std :: cout << "fri" << std :: endl;
+			break;
+		case 5:
+			std :: cout << "sat" << std :: endl;
+			break;
+		default:
+			std :: cout << "sun" << std :: endl;
+	}
+	// <expression> ? <action 1> : <action 2>
+	// true - action 1
+	// false - action 2
+	return 0;
+}
